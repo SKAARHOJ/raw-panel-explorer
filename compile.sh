0c5f745c157f6649aebd972267d6f326b15a8892
Kasper Skårhøj <kasper@skaarhoj.com>
2022-08-06 13:43:14 +0200

    Updated compile script

--- compile.sh
+++ compile.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 go build -o binaries/PanelExplorer
+GOOS=darwin GOARCH=arm64 go build -o binaries/PanelExplorer.Mac-ARM
 GOOS=windows GOARCH=amd64 go build -o binaries/PanelExplorer.exe
 
 cd binaries
 
-zip PanelExplorer.Mac.zip PanelExplorer
+zip PanelExplorer.Mac.zip PanelExplorer PanelExplorer.Mac-ARM
 zip PanelExplorer.Win.zip PanelExplorer.exe
 
 cd ..
